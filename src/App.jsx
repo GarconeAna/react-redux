cb181a4a2ae3ff6d9736f0c598f777eb601e3486
Ana Carolina <garcone.anaxx@gmail.com>
2022-08-29 22:16:03 -0300

    new project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App = () => {
+  return <>
+  <div>
+    <h1>counter</h1>
+    <button>DECREMENT</button>
+    <button>INCREMENT</button>
+  </div>
+  </>
+}
+
+export default App;
