abf4f32f35403ef502f8a6786a45c4051607f47f
Ana Carolina <garcone.anaxx@gmail.com>
2022-08-29 22:44:53 -0300

    project counter

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { increment, decrement } from "./action";
 
 const App = () => {
+  const counter = useSelector((state) => state);
+  const dispatch = useDispatch();
   return <>
   <div>
-    <h1>counter</h1>
-    <button>DECREMENT</button>
-    <button>INCREMENT</button>
+    <h1>{counter}</h1>
+    <button onClick={() => dispatch(decrement())}>DECREMENT</button>
+    <button onClick={() => dispatch(increment())}>INCREMENT</button>
   </div>
   </>
 }
